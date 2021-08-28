6904f5e66b3b7eeb9e1c8a43746cc835d777c7f8
xichuanwushao <avatarwx@163.com>
2021-08-28 10:56:49 +0800

    5-6 封装全局变量和全局方法 向后端发出ajax请求 完成超级管理员注册 1.定义全局路径和封装Ajax（移动端）

--- main.js
+++ main.js
@@ -9,3 +9,52 @@ const app = new Vue({
     ...App
 })
 app.$mount()
+//封装全局变量
+let baseUrl = "http://127.0.0.1:8080/emos";
+Vue.prototype.url={
+	register:baseUrl+"/user/register"
+}
+//全局方法名字 ajax 
+// url 请求地址
+// method 请求方式
+// data 请求提交数据
+// fun 匿名函数
+Vue.prototype.ajax = function(url, method, data, fun) {
+	uni.request({
+		"url": url,
+		"method": method,
+		"header": {
+			//请求头 值从本地store取 使用同步调用
+			token: uni.getStorageSync("token")
+		},
+		"data": data,
+		//写一个success回调 resp是响应
+		success: function(resp) {
+			//401用户没有登录 跳转登录页面
+			if (resp.statusCode == 401) {
+				uni.redirectTo({
+					url: "/pages/login/login.vue"
+				})
+				//http的状态码statusCode和业务的状态码code
+			} else if (resp.statusCode == 200 && resp.data.code == 200) {
+				//取出响应的数据
+				let data = resp.data
+				//判断响是否包含某一属性 令牌
+				if (data.hasOwnProperty("token")) {
+					let token = data.token
+					//将token保存到Storage 调用同步方法
+					uni.setStorageSync("token", token)
+				}
+				//将参数传入的匿名函数执行一下 将响应对象作为参数传入匿名函数 
+				//匿名函数从响应里面提取数据 显示在页面上面
+				fun(resp)
+			} else {
+				//后端项目执行发现异常 返回异常后 移动端需要显示异常消息
+				uni.showToast({
+					icon: "none",//面包屑
+					title: resp.data//面包屑内容
+				})
+			}
+		},
+	})
+}
