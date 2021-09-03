2ded4695e4a59b286acd0a1d6f1e26242ebe8805
xichuanwushao <avatarwx@163.com>
2021-09-03 15:55:04 +0800

    8-2 创建页面 引入less文件 并排版布局 1.编写签到成功页面 创建页面

---
+++ pages/checkin_result/checkin_result.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="page">
+		<view class="summary-container">
+			<view class="user-info"><!--插值用户的基本信息 使用flex布局 水平排列-->
+				<image :src="photo" mode="widthFix" class="photo"></image><!--放置头像-->
+				<view class="info">
+					<text class="username">{{name}}</text>
+					<text class="dept">隶属部门：{{deptName==null?"":deptName}}</text>
+				</view>
+			</view>
+			<view class="date">{{date}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {//定义变量
+				
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("checkin_result.less");
+</style>
+
