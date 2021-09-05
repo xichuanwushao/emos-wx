89feab552cf5aba6e3cba894a66251fd0bf72031
xichuanwushao <avatarwx@163.com>
2021-09-05 13:13:00 +0800

    8-5 编写签到成功页面 中间部分

--- pages/checkin_result/checkin_result.vue
+++ pages/checkin_result/checkin_result.vue
@@ -12,15 +12,38 @@
 		</view>
 		<view class="result-container">
 			<view class="left">
-							<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
-							<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
-							<view class="line"></view>
+				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+				<view class="line"></view>
 			</view>
-			<view class="right"></view>
+			<view class="right">
+				<view class="row">
+					<text class="start">上班（时间 {{ attendanceTime }}）</text>
+				</view>
+				<view class="row">
+					<text class="checkin-time">签到时间（{{ checkinTime }}）</text>
+					<text class="checkin-result green other" v-if="status=='正常'">{{status}}</text>
+					<text class="checkin-result yellow other" v-if="status=='迟到'">{{status}}</text>
+				</view>
+				<view class="row">
+					<image src="../../static/icon-7.png" mode="widthFix" class="icon-small"></image>
+					<text class="desc">{{address}}</text>
+					<text class="checkin-result green" v-if="risk=='低风险'">{{risk}}</text>
+					<text class="checkin-result yellow" v-if="risk=='中风险'">{{risk}}</text>
+					<text class="checkin-result red" v-if="risk=='高风险'">{{risk}}</text>
+				</view>
+				<view class="row">
+					<image src="../../static/icon-8.png" mode="widthFix" class="icon-small"></image>
+					<text class="desc">身份验证</text>
+					<text class="checkin-result green">已通过</text>
+				</view>
+				<view class="row">
+					<text class="end">下班（时间 {{ closingTime }}）</text>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
