1ce5377a22e62929a82caec84a1d5acbecc16f65
xichuanwushao <avatarwx@163.com>
2021-09-06 15:02:55 +0800

    8-12 创建文件 1.设计我的考勤页面

---
+++ pages/my_checkin/my_checkin.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	import uniCalendar from '@/components/uni-calendar/uni-calendar.vue';
+	export default {
+		components: {
+					uniCalendar
+		},
+		data() {
+			return {
+				list: [],
+				sum_1: 0,
+				sum_2: 0,
+				sum_3: 0
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url('my_checkin.less');
+</style>
