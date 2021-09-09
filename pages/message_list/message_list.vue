92bfe34e16589b6866a4e6d9f695b77e0bc3387e
xichuanwushao <avatarwx@163.com>
2021-09-09 12:58:26 +0800

    9-14 设计系统消息列表页面 参考网址:https://ext.dcloud.net.cn/plugin?id=24

--- pages/message_list/message_list.vue
+++ pages/message_list/message_list.vue
@@ -1,7 +1,16 @@
 <template>
-	<view>
-		
-	</view>
+	<view class="page">
+			<uni-list><!-- title标题  note消息内容 avatar头像 badgePositon小红球位置左 badgeText是否显示小红球 dot小红球 link点击跳转页面-->
+				<uni-list-chat v-for="one in list" :key="one.id" :title="one.senderName"
+					:note="one.msg" :avatar="one.senderPhoto" badgePositon="left"
+					:badgeText="one.readFlag?'':'dot'" link="navigateTo"
+					:to="'../message/message?id=' + one.id + '&readFlag=' + one.readFlag + '&refId=' + one.refId">
+					<view class="chat-custom-right">
+						<text class="chat-custom-text">{{one.sendTime}}</text>
+					</view>
+				</uni-list-chat>
+			</uni-list>
+		</view>
 </template>
 
 <script>
@@ -17,15 +26,15 @@
 			return {
 				page:1,
 				length:20,
-				list:[
+				list:[{
 					 id: "87737874656661504",
 					 refId:"89995738783813632",
-					 senderName:"Emos系统",
+					 senderName:"系统消息",
 					 senderPhoto:"https://thirdwx.qlogo.cn/mmopen/vi_32/mJQ68h2NfxXIm62ibSB9X8dRM8NCTZvFsW9Cv34B2pwKGJuylOn11picMDCgFsjxGiaWq71xTZia1uCfatDAWr2U5g/132",
 					 msg:"HelloWorld",
 					 readFlag:false,
 					 sendTime: "2021-01-23 17:21:30"
-				],
+				}],
 				isLastPage:false
 			}
 		},
@@ -35,6 +44,6 @@
 	}
 </script>
 
-<style>
-
+<style lang="less">
+	@import url("message_list.less");
 </style>
