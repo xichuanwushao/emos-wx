be1539637764b92cd3f2aaf22650ecbf0d3d648c
xichuanwushao <avatarwx@163.com>
2021-09-09 16:28:29 +0800

    9-16 定义消息页面样式 1.实现系统消息页面

---
+++ pages/message/message.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="page">
+		<view class="message">
+			<view class="header">
+				<view class="desc">{{sendTime}}</view>
+				<view class="opt" @tap="deleteMsg()">删除</view>
+			</view>
+			<view class="content">{{msg}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
